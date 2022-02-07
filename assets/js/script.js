8d232bc7e607fd510aebd69d52518f43c0ad95b5
Clarkinr <robclarkin1@gmail.com>
2022-02-07 20:26:00 +0000

    add basic JS function structure

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+let wins = 0;
+let losses = 0;
+
+let wins_num = document.getElementById("wins");
+let losses_num = document.getElementById("losses");
+
+
+
+function rungame() {
+
+}
+
+function generateComputerAnswer() {
+
+}
+
+function checkWinner() {
+
+}
+
+function userWinner() {
+
+}
+
+function computerWinner() {
+    
+}
