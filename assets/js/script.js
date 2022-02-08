5de6fc677969d0b7ef63c1956698beb990fae312
Clarkinr <robclarkin1@gmail.com>
2022-02-08 11:33:06 +0000

    add event listeners for buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+    button.addEventListener('click', function() {
+        if (this.getAttribute("data-type") === "rock") {
+            alert("you have chosen rock");
+        } else if (this.getAttribute("data-type") === "paper") {
+            alert("you have chosen paper");
+        } else if (this.getAttribute("data-type") === "scissors") {
+            alert("you have chosen scissors");
+        } else if (this.getAttribute("data-type") === "lizard") {
+            alert("you have chosen lizard");
+        } else if (this.getAttribute("data-type") === "spock") {
+            alert("you have chosen spock");
+        }
+    })    
+    } 
+})
+
+
+
 let wins = 0;
 let losses = 0;
 
@@ -5,7 +27,7 @@ let wins_num = document.getElementById("wins");
 let losses_num = document.getElementById("losses");
 let userChoice = "";
 let computerChoice = "";
-let buttons = document.getElementsByTagName("button");
+
 
 
 
