d44f0c3b626b7d53a1c678847def1ff1a3481a5c
Clarkinr <robclarkin1@gmail.com>
2022-02-08 12:28:41 +0000

    edit event listeners to change user-select id tag when button clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,29 @@
+
+let compChoiceDisplay = document.getElementById("comp-select");
+
+/**
+ * Add event listeners to buttons which will change the inner html of span id user-select
+ */
+
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-    button.addEventListener('click', function() {
-        if (this.getAttribute("data-type") === "rock") {
-            alert("you have chosen rock");
-        } else if (this.getAttribute("data-type") === "paper") {
-            alert("you have chosen paper");
-        } else if (this.getAttribute("data-type") === "scissors") {
-            alert("you have chosen scissors");
-        } else if (this.getAttribute("data-type") === "lizard") {
-            alert("you have chosen lizard");
-        } else if (this.getAttribute("data-type") === "spock") {
-            alert("you have chosen spock");
-        }
-    })    
-    } 
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "rock") {
+               document.getElementById("user-select").innerHTML = 'USER: ROCK';
+            } else if ((this.getAttribute("data-type") === "paper")) {
+                document.getElementById("user-select").innerHTML = 'USER: PAPER';
+            } else if ((this.getAttribute("data-type") === "scissors")) {
+                document.getElementById("user-select").innerHTML = 'USER: SCISSORS';
+            } else if ((this.getAttribute("data-type") === "lizard")) {
+                    document.getElementById("user-select").innerHTML = 'USER: LIZARD';
+            } else if ((this.getAttribute("data-type") === "spock")) {
+                    document.getElementById("user-select").innerHTML = 'USER: SPOCK';
+            }
+        })
+    }
 })
-
-
-
-let wins = 0;
-let losses = 0;
-
-let wins_num = document.getElementById("wins");
-let losses_num = document.getElementById("losses");
-let userChoice = "";
-let computerChoice = "";
-
 
 
 
