a43c117060e70e9038d799ee8eb711170b6939e8
Clarkinr <robclarkin1@gmail.com>
2022-02-08 14:06:32 +0000

    add call for generateComputerAnswer function and add tester html write to function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
-
-let compChoiceDisplay = document.getElementById("comp-select");
-
+let computerChoice = "";
 /**
  * Add event listeners to buttons which will change the inner html of span id user-select
+ * and start game
  */
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -11,18 +10,19 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "rock") {
-               document.getElementById("user-select").innerHTML = 'USER: ROCK';
+               document.getElementById("user-select").innerHTML = 'USER: <i class="far fa-hand-rock"></i>';
             } else if ((this.getAttribute("data-type") === "paper")) {
-                document.getElementById("user-select").innerHTML = 'USER: PAPER';
+                document.getElementById("user-select").innerHTML = 'USER: <i class="far fa-hand-paper"></i>';
             } else if ((this.getAttribute("data-type") === "scissors")) {
-                document.getElementById("user-select").innerHTML = 'USER: SCISSORS';
+                document.getElementById("user-select").innerHTML = 'USER: <i class="far fa-hand-scissors"></i>';
             } else if ((this.getAttribute("data-type") === "lizard")) {
-                    document.getElementById("user-select").innerHTML = 'USER: LIZARD';
-            } else if ((this.getAttribute("data-type") === "spock")) {
-                    document.getElementById("user-select").innerHTML = 'USER: SPOCK';
-            }
+                document.getElementById("user-select").innerHTML = 'USER: <i class="far fa-hand-lizard"></i>';
+            } else {
+                document.getElementById("user-select").innerHTML = 'USER: <i class="far fa-hand-spock"></i>';
+            } 
+            generateComputerAnswer();
         })
-    }
+    } 
 })
 
 
@@ -32,16 +32,10 @@ function rungame() {
 }
 
 function generateComputerAnswer() {
-
+    document.getElementById("comp-select").innerHTML = 'CPU: <i class="far fa-hand-rock"></i>';
 }
 
-function displayComputerAnswer() {
 
-}
-
-function displayUserAnswer() {
-    
-}
 
 function checkWinner() {
 
