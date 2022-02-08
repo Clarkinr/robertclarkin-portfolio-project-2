0ccf7635fa419aba8cdc435f23b5761fbb54a680
Clarkinr <robclarkin1@gmail.com>
2022-02-08 15:03:11 +0000

    add score definitions for userScore and compScore to button function and computerAnswer fucntion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-let computerChoice;
+let userScore;
+let compScore;
 /**
  * Add event listeners to buttons which will change the inner html of span id user-select
  * and start game
@@ -11,44 +12,57 @@ document.addEventListener("DOMContentLoaded", function() {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "rock") {
                document.getElementById("user-select").innerHTML = 'USER: <i class="far fa-hand-rock"></i>';
+               userScore = "rock";
             } else if ((this.getAttribute("data-type") === "paper")) {
                 document.getElementById("user-select").innerHTML = 'USER: <i class="far fa-hand-paper"></i>';
+                userScore = "paper";
             } else if ((this.getAttribute("data-type") === "scissors")) {
                 document.getElementById("user-select").innerHTML = 'USER: <i class="far fa-hand-scissors"></i>';
+                userScore = "scissors";
             } else if ((this.getAttribute("data-type") === "lizard")) {
                 document.getElementById("user-select").innerHTML = 'USER: <i class="far fa-hand-lizard"></i>';
+                userScore = "lizard";
             } else {
                 document.getElementById("user-select").innerHTML = 'USER: <i class="far fa-hand-spock"></i>';
+                userScore = "spock";
             } 
             generateComputerAnswer();
+            runGame();
         })
     } 
 })
 
-
-
-function rungame() {
-
-}
 /**
  * Generates a random choice for the computer and
  * enters the random choice into the span id comp-select 
  */
-function generateComputerAnswer() {
+ function generateComputerAnswer() {
     this.computerChoice = Math.random();
     if (this.computerChoice < 0.2) {
         document.getElementById("comp-select").innerHTML = 'CPU: <i class="far fa-hand-rock"></i>';
+        compScore = "rock";
     } else if (this.computerChoice <= 0.4) {
         document.getElementById("comp-select").innerHTML = 'CPU: <i class="far fa-hand-paper"></i>';
+        compScore = "paper";
     } else if (this.computerChoice <= 0.6) {
         document.getElementById("comp-select").innerHTML = 'CPU: <i class="far fa-hand-scissors"></i>';
+        compScore = "scissors";
     } else if (this.computerChoice <= 0.8) {
         document.getElementById("comp-select").innerHTML = 'CPU: <i class="far fa-hand-lizard"></i>';
+        compScore  = "lizard";
     } else {
         document.getElementById("comp-select").innerHTML = 'CPU: <i class="far fa-hand-spock"></i>';
-    }
+        compScore  = "spock";
+    } 
 }
 
+function runGame() {
+    if (userScore === compScore) {
+        console.log("Game is a draw");
+    } else {
+        console.log("Game is not a Draw");
+    }
+}
 
 
 function checkWinner() {
